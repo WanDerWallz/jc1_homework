1245e515b80387c5cfa549bf0c55df021c4eb3b9
WanDerWallz <glebkoroteev33@gmail.com>
2023-03-19 16:13:28 +0300

    Added homework tasks 1-2

--- src/Par1/Task2/Clock.java
+++ src/Par1/Task2/Clock.java
@@ -5,7 +5,7 @@ public class Clock {
         int s, sec, m , min, h, hour, d, day, week;
         s = 3700;
         sec = s % 60;
-        m = (s -sec) / 60;
+        m = (s - sec) / 60;
         min = m % 60;
         h = (m - min) / 60;
         hour = h % 24;
