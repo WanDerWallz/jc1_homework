d5ca62a34059e44dfb9e5fe0679885dca409e6be
WanDerWallz <glebkoroteev33@gmail.com>
2023-03-26 15:18:16 +0300

    Added 3 tasks of Part 2 (4-6)

---
+++ src/Part2/Task1/DoubleSum.java
@@ -0,0 +1,7 @@
+package Part2.Task1;
+
+public class DoubleSum {
+    private int doubleSum(int a, int b) {
+        return a + b + a * b;
+    }
+}
---
+++ src/Part2/Task1/Variables.java
@@ -0,0 +1,16 @@
+package Part2.Task1;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte b = 0x55;
+        short s = 0x55ff;
+        int i = 1000000;
+        long l = 0xffffffffL;
+        char c = 'a';
+        float f = .25f;
+        double d = .00001234;
+        boolean bool = true;
+        System.out.println(b + ", " + s + ", " + i + ", " + l + ", " + c + ", " + f + ", " + d + ", " + bool + ".");
+    }
+
+}
