522e62280dc3f9a53a3670808941b6b0f6d8a0c2
WanDerWallz <125499533+WanDerWallz@users.noreply.github.com>
2023-03-26 15:12:31 +0300

    Delete src/Part1/Task1 directory

--- src/Part1/Task1/Hello_Belarus.java
+++
@@ -1,7 +0,0 @@
-package Part1.Task1;
-
-public class Hello_Belarus {
-    public static void main(String[] args) {
-        System.out.println("Hello Belarus!");
-    }
-}
--- src/Part1/Task1/I_like_java.java
+++
@@ -1,7 +0,0 @@
-package Part1.Task1;
-
-public class I_like_java {
-    public static void main(String[] args) {
-        System.out.println("Hello Belarus! I like Java!");
-    }
-}
